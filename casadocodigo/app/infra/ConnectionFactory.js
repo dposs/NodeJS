ce6db9361ee8096a88b5a2682b2d477a34ff951a
Daniel Possamai <daniel.possamai.cadastros@live.com>
2018-02-06 00:20:08 -0200

    1.13

--- casadocodigo/app/infra/ConnectionFactory.js
+++ casadocodigo/app/infra/ConnectionFactory.js
@@ -19,6 +19,14 @@ class ConnectionFactory {
               password: "root",
               database:"casadocodigo_test"
       });
+
+    if (process.env.NODE_ENV == "production") {
+      return mysql.createConnection({
+              host: 'us-cdbr-iron-east-03.cleardb.net',
+              user:'cdd75da8535f66',
+              password:'g2a35b88',
+              database:'heroku_db9452fds6ahdog'
+      });
     }
   }
 }
