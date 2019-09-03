594ab065c81c833e6621a49658271d83f383c47e
Daniel Possamai <daniel.possamai.cadastros@live.com>
2019-09-02 23:40:58 -0300

    2019-09-02

--- GPoster/src/service/ImdbService.js
+++ GPoster/src/service/ImdbService.js
@@ -1,8 +1,4 @@
-let superagent = require("superagent");
-let xmldom = require("xmldom");
-let xpath = require("xpath");
-
-let DOMParser = xmldom.DOMParser;
+let osmosis = require("osmosis");
 
 /**
  * Service de comunicação com IMDB.
@@ -18,19 +14,18 @@ class ImdbService {
    * @memberof ImdbService
    */
   async getCoverUrl(url) {
-    let response = await superagent.get(url);
-    let html = response.res.text.replace(/\n/g, "");
+    let coverUrl;
 
-    let noErrors = {
-      "errorHandler": {
-        "warning": function() {}, 
-        "error": function() {}, 
-        "fatalError": function() {}
-      }
-    };
-
-    var doc = new DOMParser(noErrors).parseFromString(html);
-    return xpath.select("string(//div[contains(concat('$', @class, '$'), '$poster$')]/a/img/@src)", doc);
+    await osmosis
+      .get(url)
+      .find("//div[contains(concat('$', @class, '$'), '$poster$')]/a/img/@src")
+      .set("coverUrl")
+      .data(data => coverUrl = data.coverUrl)
+      .log(console.log)
+      .error(console.log)
+      .debug(console.log);
+    
+    return coverUrl;
   }
 }
 
--- GPoster/src/service/TorrentFreakService.js
+++ GPoster/src/service/TorrentFreakService.js
@@ -1,8 +1,4 @@
-let superagent = require("superagent");
-let xmldom = require("xmldom");
-let xpath = require("xpath");
-
-let DOMParser = xmldom.DOMParser;
+let osmosis = require("osmosis");
 
 let Movie = require("../data/Movie");
 
@@ -22,32 +18,32 @@ class TorrentFreakService {
    * @memberof TorrentFreakService
    */
   async getTopWeekMovies(url) {
-    let response = await superagent.get(url);
-    let html = response.res.text.replace(/\n/g, "");
-
-    let noErrors = {
-      "errorHandler": {
-        "warning": function() {}, 
-        "error": function() {}, 
-        "fatalError": function() {}
-      }
-    };
-
-    var doc = new DOMParser(noErrors).parseFromString(html);
-    var rows = xpath.select("//div[contains(@class, 'entry-content')]/table/tbody/tr", doc);
-    
     let movies = new Array();
 
-    for (let row of rows) {
-      movies.push(new Movie()
-        .setName(xpath.select("string(td[3])", row))
-        .setRank(xpath.select("string(td[1])", row))
-        .setRankLast(xpath.select("string(td[2])", row))
-        .setImdbScore(xpath.select("string(td[4]/a[1])", row))
-        .setImdbUrl(xpath.select("string(td[4]/a[1]/@href)", row))
-        .setYoutubeUrl(xpath.select("string(td[4]/a[2]/@href)", row))
-      );
-    }
+    await osmosis
+      .get(url)
+      .find("//div[contains(@class, 'entry-content')]/table/tbody/tr")
+      .set({
+        "name": "td[3]",
+        "rank": "td[1]",
+        "rankLast": "td[2]",
+        "imdbScore": "td[4]/a[1]",
+        "imdbUrl": "td[4]/a[1]/@href",
+        "youtubeUrl": "td[4]/a[2]/@href",
+      })
+      .data(data => {
+        movies.push(new Movie()
+          .setName(data.name)
+          .setRank(data.rank)
+          .setRankLast(data.rankLast)
+          .setImdbScore(data.imdbScore)
+          .setImdbUrl(data.imdbUrl)
+          .setYoutubeUrl(data.youtubeUrl)
+        );
+      })
+      .log(console.log)
+      .error(console.log)
+      .debug(console.log);
 
     return movies;
   }
